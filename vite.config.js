0aae73c8d2e423a0edad30bed6a36a9f8812f7e8
Sebastian Doerl <sebastian.doerl@ottogroup.com>
2023-05-08 13:20:48 +0200

    Update build config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,36 @@
+import * as path from 'node:path'
 import { fileURLToPath, URL } from 'node:url'
+import vue from '@vitejs/plugin-vue'
 
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    build: {
+        lib: {
+            entry: path.resolve(__dirname, 'src/install.js'),
+            name: 'v-tail',
+            fileName: format => `v-tail.${format}.js`
+        },
+        rollupOptions: {
+            external: ['vue'],
+            output: {
+                globals: {
+                    'vue': 'Vue'
+                }
+            }
+        },
+        minify: false
+    },
+    optimizeDeps: {
+        exclude: ['vue-demi']
+    },
+    plugins: [
+        vue()
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
     }
-  }
 })

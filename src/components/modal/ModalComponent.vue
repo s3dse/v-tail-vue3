0fc2633a23d5150d5ea5e6f989fc3011267aad5f
Sebastian Doerl <sebastian.doerl@ottogroup.com>
2023-09-25 13:10:00 +0200

    Dark modal

--- src/components/modal/ModalComponent.vue
+++ src/components/modal/ModalComponent.vue
@@ -9,11 +9,7 @@
         <div class="un-fixed un-inset-0">
             <div class="un-flex un-min-h-full un-items-center un-justify-center un-text-center">
                 <DialogPanel :class="dialogPanelClassList">
-<<<<<<< HEAD
-                    <div class="un-border-b un-bg-white">
-=======
                     <div class="un-border-b un-bg-white dark:un-border-moon-700">
->>>>>>> 5c1882db6b10f8c17454daf8c27c53b14e593a4d
                         <DialogTitle as="div" :class="dialogTitleClassList">
                             {{ title }}
                         </DialogTitle>
@@ -21,11 +17,7 @@
                     <div :class="bodyClassList">
                         <slot name="content">
                             <div class="un-mt-2 un-px-4">
-<<<<<<< HEAD
-                                <p class="un-text-sm un-text-gray-500">
-=======
                                 <p class="un-text-sm un-text-gray-500 dark:text-gray-100">
->>>>>>> 5c1882db6b10f8c17454daf8c27c53b14e593a4d
                                     Lorem Ipsum Something Something
                                 </p>
                             </div>
@@ -82,31 +74,20 @@ const props = defineProps({
         default: joinLines(`un-fixed 
                             un-inset-0 
                             un-bg-black 
-<<<<<<< HEAD
-                            un-bg-opacity-25`)
-=======
                             dark:un-bg-moon-900
                             un-bg-opacity-25
                             dark:un-bg-opacity-85`)
->>>>>>> 5c1882db6b10f8c17454daf8c27c53b14e593a4d
     },
     dialogPanelClassList: {
         type: String,
         default: joinLines(`un-w-full 
                             un-max-w-md 
                             un-overflow-hidden 
-<<<<<<< HEAD
-                            un-bg-white 
-                            
-                            un-text-left 
-                            un-align-middle 
-=======
                             un-bg-white
                             dark:un-bg-moon-600
                             un-rounded
                             un-text-left 
                             un-align-middle
->>>>>>> 5c1882db6b10f8c17454daf8c27c53b14e593a4d
                             un-shadow-xl`)
     },
     dialogTitleClassList: {
@@ -115,15 +96,11 @@ const props = defineProps({
                             un-text-lg 
                             un-font-medium 
                             un-leading-6 
-<<<<<<< HEAD
-                            un-text-gray-900 
-=======
                             un-text-gray-900
                             dark:un-bg-moon-700
                             dark:un-border
                             dark:un-border-moon-600
                             dark:un-text-gray-100
->>>>>>> 5c1882db6b10f8c17454daf8c27c53b14e593a4d
                             `)
     },
     title: {
@@ -132,9 +109,6 @@ const props = defineProps({
     },
     bodyClassList: {
         type: String,
-<<<<<<< HEAD
-        default: 'un-bg-slate-50'
-=======
         default: joinLines(`un-bg-slate-50 
                             dark:un-bg-moon-800 
                             dark:un-text-gray-100 
@@ -144,7 +118,6 @@ const props = defineProps({
                             dark:un-border-b-moon-600 
                             dark:un-border-l-moon-600 
                             dark:un-border-r-moon-600`)
->>>>>>> 5c1882db6b10f8c17454daf8c27c53b14e593a4d
     },
     controlSectionClassList: {
         type: String,

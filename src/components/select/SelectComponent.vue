6c9c1bb9ba0d3832ece44dcf125fe1e534378f6a
Sebastian Doerl <sebastian.doerl@ottogroup.com>
2023-10-31 14:32:29 +0100

    Increase z index of select

--- src/components/select/SelectComponent.vue
+++ src/components/select/SelectComponent.vue
@@ -23,7 +23,7 @@ const classes = {
         `un-inline-flex un-items-center un-justify-between un-gap-2 un-min-w-[10rem] 
         un-bg-navy-500 un-h-[2.375rem] un-rounded un-text-gray-100 un-px-3`,
     content: `un-bg-white dark:un-bg-moon-800 un-rounded un-min-w-[11.25rem] 
-        un-shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)]`,
+        un-shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] un-z-130`,
     item: `un-text-gray-900 dark:un-text-gray-100 un-leading-none un-flex un-items-center un-pl-8
         un-outline-none un-select-none un-py-1 data-[disabled]:un-cursor-not-allowed hover:un-bg-gray-200 dark:hover:un-bg-moon-600`
 }

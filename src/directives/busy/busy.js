951bcd0ee0cf35a03fcdbd24858b5a02b791555a
Sebastian Doerl <sebastian.doerl@ottogroup.com>
2023-11-30 08:57:20 +0100

    Set z-index of v-busy-elements

--- src/directives/busy/busy.js
+++ src/directives/busy/busy.js
@@ -1,6 +1,6 @@
 // @unocss-include
 
-const busyClasses = ['busy-text-navy-500', 'busy-bg-slate-50', 'dark:busy-text-navy-200', 'dark:busy-bg-slate-900']
+const busyClasses = ['busy-text-navy-500', 'busy-bg-slate-50', 'dark:busy-text-navy-200', 'dark:busy-bg-slate-900', 'z-0']
 export const busy = (el, binding) => {
     if (binding.value) {
         el.dataset.busy = true

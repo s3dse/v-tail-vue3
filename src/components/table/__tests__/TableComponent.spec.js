adaf8f3bbf88f41590b1efc7376989dc3977bc81
Sebastian Doerl <sebastian.doerl@ottogroup.com>
2024-03-06 15:41:34 +0100

    Test DOM

--- src/components/table/__tests__/TableComponent.spec.js
+++ src/components/table/__tests__/TableComponent.spec.js
@@ -44,44 +44,52 @@ describe('TableComponent without top rows', () => {
 
 describe('TableComponent', async () => {
     const items = [...Array(100).keys()].map(k => ({ name: `item-${k}`, value: k }))
-    const shuffledItems = items.sort(() => 0.5 - Math.random())
+    const shuffledItems = [...items].sort(() => 0.5 - Math.random())
     const props = {
         perPage: 10,
         topRows: [],
         items: shuffledItems,
         fields: [{ key: 'name' }, { key: 'value' }]
     }
+    const intValuesOfColumn = (wrapper, column) =>
+        wrapper
+            .findAll(`td:nth-child(${column})`)
+            .map(domWrapper => domWrapper.text())
+            .map(value => parseInt(value))
     it('should sort ascendingly on first column-click', async () => {
         const wrapper = shallowMount(TableComponent, { props })
         const el = wrapper.findAll('th')[1]
         await el.find('.i-tabler-arrows-sort').trigger('click')
-        await nextTick()
         expect(wrapper.vm.tableData.map(td => td.value)).toStrictEqual([...Array(100).keys()])
+        const values = intValuesOfColumn(wrapper, 2)
+        expect(values).toStrictEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
     })
     it('should sort descendingly on second column-click', async () => {
         const wrapper = shallowMount(TableComponent, { props })
         const el = wrapper.findAll('th')[1]
         await el.find('.i-tabler-arrows-sort').trigger('click')
-        await nextTick()
         await wrapper.findAll('th')[1].find('.i-tabler-sort-ascending').trigger('click')
-        await nextTick()
         expect(wrapper.vm.tableData.map(td => td.value)).toStrictEqual(
             [...Array(100).keys()].reverse()
         )
+        const values = intValuesOfColumn(wrapper, 2)
+        expect(values).toStrictEqual([99, 98, 97, 96, 95, 94, 93, 92, 91, 90])
     })
     it('should filter data', async () => {
-        const wrapper = shallowMount(TableComponent, { props })
+        const wrapper = shallowMount(TableComponent, { props: { ...props, items } })
         const filterInput = wrapper.find('input')
         await filterInput.setValue('1')
-        console.log(wrapper.vm.tableData)
         expect(wrapper.vm.tableData.map(td => td.value).sort()).toStrictEqual([
             1, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 21, 31, 41, 51, 61, 71, 81, 91
         ])
+        const values = intValuesOfColumn(wrapper, 2)
+        expect(values).toStrictEqual([1, 10, 11, 12, 13, 14, 15, 16, 17, 18])
     })
-    it('should filter an empty result when no match', async() => {
+    it('should filter an empty result when no match', async () => {
         const wrapper = shallowMount(TableComponent, { props })
         const input = wrapper.find('input')
         await input.setValue('test')
         expect(wrapper.vm.tableData).toStrictEqual(new Array())
+        expect(wrapper.findAll('td')).toStrictEqual(new Array())
     })
 })

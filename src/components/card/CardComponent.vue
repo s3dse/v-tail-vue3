7841d26d9861fc23716200ab36e87e055bc3f0f2
Sebastian Doerl <sebastian.doerl@ottogroup.com>
2024-05-06 16:57:24 +0200

    Remove card title

--- src/components/card/CardComponent.vue
+++ src/components/card/CardComponent.vue
@@ -1,16 +1,5 @@
 <template>
     <div class="un-card" :class="classes">
-        <slot name="title">
-            <div class="un-py-3 un-flex un-flex-col un-gap-1 un-border-b dark:un-border-moon-700">
-                <div class="un-flex un-flex-col un-gap-1 un-ms-4 un-me-1">
-                    <div class="un-flex un-gap-1 un-items-baseline">
-                        <div class="un-card-title">
-                        {{ title }}
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </slot>
         <slot></slot>
     </div>
 </template>
@@ -22,10 +11,6 @@ export default {
         classes: {
             type: String,
             default: 'dark:un-bg-moon-800 dark:un-border-moon-700'
-        },
-        title: {
-            type: String,
-            default: null
         }
     },
     name: 'card-component'

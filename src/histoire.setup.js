564fbf1e37bb592eb0b5e6674ea696181f4678e9
Sebastian Doerl <sebastian.doerl@ottogroup.com>
2024-07-10 09:13:05 +0200

    Introduce Histoire

---
+++ src/histoire.setup.js
@@ -0,0 +1,9 @@
+import { defineSetupVue3 } from "@histoire/plugin-vue";
+import { busy } from '@/directives'
+import '@unocss/reset/tailwind.css'
+import 'virtual:uno.css'
+
+export const setupVue3 = defineSetupVue3(({ app, story, variant }) => {
+
+    app.directive('busy', busy)
+})

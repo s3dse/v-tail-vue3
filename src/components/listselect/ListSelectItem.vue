a714b36ffdade09736ac6ea2ed5aa21a4e46d3b3
Sebastian Doerl <sebastian.doerl@ottogroup.com>
2025-03-31 09:25:00 +0200

    Add scope for combined data selector

--- src/components/listselect/ListSelectItem.vue
+++ src/components/listselect/ListSelectItem.vue
@@ -4,7 +4,8 @@
         class="listselectitem un-w-full un-h-full un-ps-2 un-py-2 hover:un-cursor-pointer
         un-text-gray-900 dark:un-text-gray-100 
         data-[state=checked]:un-bg-primary data-[state=checked]:un-text-gray-100 
-        data-[highlighted]:un-text-gray-100 data-[highlighted]:un-bg-primary-400"
+        data-[highlighted]:un-text-gray-100 data-[highlighted]:un-bg-primary-400
+        [&[data-state=checked][data-highlighted]]:un-bg-red-400"
     >
         {{ props.labelFn(option) }}
     </div>
@@ -17,9 +18,3 @@ const props = defineProps({
     truncateItems: {type: Boolean, default: false},
 });
 </script>
-
-<style scoped>
-.listselectitem[data-state="checked"][data-highlighted] {
-    --at-apply: 'un-bg-red-400'
-}
-</style>
